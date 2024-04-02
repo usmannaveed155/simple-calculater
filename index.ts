a2de6fa736d034cedb06b4e3ee3dfd2b88c08c87
usman <usmannaveed155@gmail.com>
2024-04-02 23:21:30 +0500

    simple calculater

---
+++ index.ts
@@ -0,0 +1,28 @@
+#!/usr/bin/evn node
+import inquirer from "inquirer";
+
+const asnwer = await inquirer.prompt([
+  { message: "Enter fisrt number", type: "number", name: "fisrtNumber" },
+  { message: "Enter second number", type: "number", name: "secondNumber" },
+  {
+    message: "Select one of the operators to perform action",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction","Multiplication", "Division" ],
+  },
+]);
+
+ //conditional statement
+ if (asnwer.operator === "Addition") {
+  console.log(asnwer.fisrtNumber + asnwer.secondNumber);
+ } else if (asnwer.operator === "Subtraction") {
+  console.log(asnwer.fisrtNumber - asnwer.secondNumber);
+ } else if (asnwer.operator === "Multiplication") {
+  console.log(asnwer.fisrtNumber * asnwer.secondNumber);
+ } else if (asnwer.operator === "Division") {
+  console.log(asnwer.fisrtNumber / asnwer.secondNumber);
+ } else {
+  console.log("Invalid operator");
+ }
+
+// console.log(asnwer);
